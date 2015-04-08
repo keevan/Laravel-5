44bec90e613ed7ca9909e20d1acecdbe854d8f65
keevan <keevan.pham@gmail.com>
2015-04-08 21:11:21 +1000

    -Eloquent relationships (hasMany, hasOne) -foreign key assignment inside migration schema -fixed username (from user) in User.php

--- app/Article.php
+++ app/Article.php
@@ -16,17 +16,44 @@ class Article extends Model {
         'published_at'
     ];
     protected $dates = ['published_at'];
+
+    /**
+     * Scope queries to articles that have published.
+     *
+     * @param $query
+     */
     public function scopePublished($query){
         $query->where('published_at','<=',Carbon::now());
 
     }
+
+    /**
+     * Scope queries to articles that haven't yet published.
+     *
+     * @param $query
+     */
     public function scopeUnpublished($query){
         $query->where('published_at','>',Carbon::now());
 
     }
+
+    /**
+     * Set the published_at attribute
+     *
+     * @param $date
+     */
     public function setPublisherAttribute($date){
         $this->attributes['published_at']=Carbon::parse($date);
 
+    }
+
+    /**
+     * An article is owned by a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo('App\User');
     }
 
 }
