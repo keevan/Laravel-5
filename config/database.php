ad725aba8a17800e0f20c55987c95b02b539d60b
keevan <keevan.pham@gmail.com>
2015-04-09 20:52:28 +1000

    -Changed database type from sqlite to mySQL database.

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => env('MYSQL_DB_HOST', 'localhost'),
+			'database'  => env('MYSQL_DB_DATABASE', 'forge'),
+			'username'  => env('MYSQL_DB_USERNAME', 'forge'),
+			'password'  => env('MYSQL_DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
